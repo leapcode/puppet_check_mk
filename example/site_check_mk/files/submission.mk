1095c6616dc9cd0aaedf611b7da3c1fc56dd2e5f
Matt Taggart <taggart@riseup.net>
2016-05-30 16:40:10 -0700

    provide an example site_check_mk with basic examples to get users started

---
+++ example/site_check_mk/files/submission.mk
@@ -0,0 +1,4 @@
+# default is "pipe", but file is more efficient (and default in newer versions)
+check_submission = "file"
+# the default in the debian package is wrong
+check_result_path = "/var/lib/nagios3/spool/checkresults/"
